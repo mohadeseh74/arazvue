e8fea0ffbfbda4197c23c2f625fb567cfd46f10a
mohammad reza ebrhimi <mr.ebrahimi@aut.ac.ir>
2017-12-25 17:52:01 +0330

    Update models.py

--- araz/gallery/models.py
+++ araz/gallery/models.py
@@ -10,6 +10,7 @@ from django.db import models
 class Gallery(models.Model):
     title = models.CharField(max_length=100, blank=False)
     image = models.ImageField(blank=False)
+    caption = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
